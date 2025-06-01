66fb8f6a99c1a549de51275331c94f37117813c5
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-06-01 09:58:36 -0300

    refactor(error): add custom error type for setup repo utility

--- seeker-hook/src/test/utils/setup_repo.rs
+++ seeker-hook/src/test/utils/setup_repo.rs
@@ -2,19 +2,33 @@ use git2::Repository;
 use std::{fs::OpenOptions, io::Write, process::Command};
 use tempfile::{tempdir, TempDir};
 
-pub fn setup_repo() -> std::io::Result<(TempDir, TempDir)> {
-    let bare_repo_dir = tempdir()?;
-    let clone_repo_dir = tempdir()?;
+#[derive(Debug)]
+pub enum SetupRepoErr {
+    Push,
+    RemoteAdd,
+    Add,
+    InitBare,
+    InitClone,
+    WriteChanges,
+    CommitChanges,
+}
+type SetupRepoResult<T> = Result<T, SetupRepoErr>;
+
+pub fn setup_repo() -> SetupRepoResult<(TempDir, TempDir)> {
+    let bare_repo_dir = tempdir().map_err(|_| SetupRepoErr::InitBare)?;
+    let clone_repo_dir = tempdir().map_err(|_| SetupRepoErr::InitClone)?;
 
     let _ = Command::new("git")
         .current_dir(bare_repo_dir.path())
         .args(["init", "--bare"])
-        .output()?;
+        .output()
+        .map_err(|_| SetupRepoErr::InitBare)?;
 
     let _ = Command::new("git")
         .current_dir(clone_repo_dir.path())
         .args(["init"])
-        .output()?;
+        .output()
+        .map_err(|_| SetupRepoErr::InitClone)?;
     let _ = Command::new("git")
         .current_dir(clone_repo_dir.path())
         .args([
@@ -23,25 +37,28 @@ pub fn setup_repo() -> std::io::Result<(TempDir, TempDir)> {
             "origin",
             bare_repo_dir.path().to_str().unwrap_or(""),
         ])
-        .output()?;
+        .output()
+        .map_err(|_| SetupRepoErr::RemoteAdd)?;
 
     let mut sample_file = OpenOptions::new()
         .write(true)
         .create(true)
         .open(clone_repo_dir.path().to_path_buf().join("doc.md"))
         .unwrap();
-    let _ = write!(sample_file, "testing")?;
+    let _ = write!(sample_file, "testing").map_err(|_| SetupRepoErr::WriteChanges)?;
     drop(sample_file);
 
     let _ = Command::new("git")
         .current_dir(clone_repo_dir.path())
         .args(["add", "doc.md"])
-        .output()?;
+        .output()
+        .map_err(|_| SetupRepoErr::Add)?;
 
     let _ = Command::new("git")
         .current_dir(clone_repo_dir.path())
         .args(["commit", "-m", "testing"])
-        .output()?;
+        .output()
+        .map_err(|_| SetupRepoErr::CommitChanges)?;
 
     let current_clone_branch_name = {
         let clone_repo = Repository::open(clone_repo_dir.path()).unwrap();
@@ -53,7 +70,8 @@ pub fn setup_repo() -> std::io::Result<(TempDir, TempDir)> {
     let _ = Command::new("git")
         .current_dir(clone_repo_dir.path())
         .args(["push", "-u", "origin", &current_clone_branch_name])
-        .output()?;
+        .output()
+        .map_err(|_| SetupRepoErr::Push)?;
 
     // TODO: add some proper error handling in case any of these commands fails
 
