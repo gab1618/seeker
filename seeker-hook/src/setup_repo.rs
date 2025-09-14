560991e4a3b6c0d63f59a0db1d2c1d7a33fdea0d
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-09-14 12:48:45 -0300

    refactor: fix typo

--- seeker-hook/src/setup_repo.rs
+++ seeker-hook/src/setup_repo.rs
@@ -10,7 +10,7 @@ pub enum SetupRepoErr {
     UpdateHead,
 }
 
-/// If repo's head does point to an inexistent ref, so it to the newly pushed one
+/// If repo's head does point to an inexistent ref, so it points to the newly pushed one
 pub fn setup_repo<P: AsRef<Path>>(mount_path: P, args: GitArgs) -> Result<(), SetupRepoErr> {
     let repo = Repository::open_bare(mount_path).map_err(|_| SetupRepoErr::OpenRepo)?;
 
