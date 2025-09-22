e0a2f7d12e4d60f6c6ee5d709fff7c32b3e3f5aa
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-09-22 19:05:37 -0300

    chore(hook): remove logging
    It makes more sense to have this in a daemon, for example. The hook
    should make its job as quick as possible, and setting up logs just to
    die in miliseconds feels like a waste.

--- seeker-hook/src/main.rs
+++ seeker-hook/src/main.rs
@@ -1,7 +1,6 @@
 use std::path::PathBuf;
 
 use changes_tracker::ChangesTracker;
-use logger::Logger;
 use setup_repo::setup_repo;
 use state_manager::StateManager;
 
@@ -12,7 +11,6 @@ mod test;
 
 mod args;
 mod changes_tracker;
-mod logger;
 mod setup_repo;
 mod state_manager;
 
@@ -28,21 +26,16 @@ fn main() -> SeekerHookResult<()> {
 
     let git_args = GitArgs::try_from(stdin_line.as_str())?;
 
-    log::info!("Starting setup");
     let repo_path: PathBuf = std::env::args().nth(1).unwrap_or(".".to_string()).into();
 
     setup_repo(&repo_path, git_args).unwrap();
 
-    Logger::setup_logging(repo_path.join("info").join("log"))?;
-
     let manager = StateManager::new(repo_path.clone());
     let tracker = ChangesTracker::new(repo_path, &manager).expect("Could not get file changes");
 
-    log::info!("Setup done. Starting the index routine");
-
     for entry in tracker.get_changed_files().unwrap() {
         let (filepath, _) = entry.unwrap();
-        log::info!("Indexing file {}", filepath.to_str().unwrap());
+        println!("Indexing file {}", filepath.to_str().unwrap());
     }
 
     Ok(())
