fd255b7332e9de7ab1b0475059509e287709d030
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2025-10-17 06:42:16 -0300

    chore(indexer): remove unused code

--- elasticsearch-indexer/src/error.rs
+++ elasticsearch-indexer/src/error.rs
@@ -2,12 +2,6 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum ElasticIndexerErr {
-    #[error("Could not generate file id")]
-    GenerateFileId,
-    #[error("Could not open file: {0}")]
-    OpenFile(#[source] std::io::Error),
-    #[error("Could not read file: {0}")]
-    ReadFile(#[source] std::io::Error),
     #[error("Could not get file name")]
     GetFileName,
 }
